47d9ab1e8c2104bbd95ebc75e01538f788ef6726
Alumno <Alumno@PC215>
2025-10-22 12:29:35 -0300

    Agregar archivos de proyecto.

---
+++ pryRomero_BarLaMilanga/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace pryRomero_BarLaMilanga
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            dgvComida = new DataGridViewTextBoxColumn();
+            dgvBebidaSinAlcohol = new DataGridViewTextBoxColumn();
+            dgvPostre = new DataGridViewTextBoxColumn();
+            dgvMozo = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { dgvComida, dgvBebidaSinAlcohol, dgvPostre, dgvMozo });
+            dataGridView1.Location = new Point(12, 85);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(444, 211);
+            dataGridView1.TabIndex = 0;
+            // 
+            // dgvComida
+            // 
+            dgvComida.HeaderText = "Comida";
+            dgvComida.Name = "dgvComida";
+            // 
+            // dgvBebidaSinAlcohol
+            // 
+            dgvBebidaSinAlcohol.HeaderText = "Bebida Sinn Alcohol";
+            dgvBebidaSinAlcohol.Name = "dgvBebidaSinAlcohol";
+            // 
+            // dgvPostre
+            // 
+            dgvPostre.HeaderText = "Postre";
+            dgvPostre.Name = "dgvPostre";
+            // 
+            // dgvMozo
+            // 
+            dgvMozo.HeaderText = "Mozo";
+            dgvMozo.Name = "dgvMozo";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn dgvComida;
+        private DataGridViewTextBoxColumn dgvBebidaSinAlcohol;
+        private DataGridViewTextBoxColumn dgvPostre;
+        private DataGridViewTextBoxColumn dgvMozo;
+    }
+}
---
+++ pryRomero_BarLaMilanga/Form1.cs
@@ -0,0 +1,10 @@
+namespace pryRomero_BarLaMilanga
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
